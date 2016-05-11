218528cbabc78c222904323e86d3bfc475a5f3ca
chadwickucla <chadwickucla@gmail.com>
2016-05-11 13:57:35 -0700

    added some tunes

---
+++ Assets/Scripts/Town/GM.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+    public AudioClip AmbientMusic;
+	// Use this for initialization
+	void Start () {
+       // gameObject.GetComponent<AudioSource>().PlayOneShot(AmbientMusic);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
