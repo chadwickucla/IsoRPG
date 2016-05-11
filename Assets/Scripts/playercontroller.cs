7e7d99199c2037cc2a80051cc67f86e00bcc842e
tommyvanduyne <tommyvanduyne@gmail.com>
2016-05-11 14:35:54 -0400

    added new scene forest forest and started writing a playercontroller

---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    private Vector3 targetPosition;
+    private Vector3 playerPosition;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+    void FixedUpdate ()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+              
+        }
+    
+        
+    }
+}
