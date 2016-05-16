4c06e7d90069c28a11a49d60f4a5fa3096b1e093
tommyvanduyne <tommyvanduyne@gmail.com>
2016-05-16 19:00:47 -0400

    edited the camera script to have a max and min multiplier for the scroll

--- Assets/Scripts/CharAndCam/CamRotate.cs
+++ Assets/Scripts/CharAndCam/CamRotate.cs
@@ -6,31 +6,40 @@ public class CamPosition
     public float x;
     public float y;
     public float z;
+    public float maxZoom;
+    public float minZoom;
 }
 
 public class CamRotate : MonoBehaviour {
 
     public Transform playerTransform;
     public CamPosition cam;
+    private float maxX, maxY, maxZ, minX, minY, minZ;
 
 	// Use this for initialization
 	void Start () {
-       
-	}
+        maxX = cam.x * cam.maxZoom;
+        maxY = cam.y * cam.maxZoom;
+        maxZ = cam.z * cam.maxZoom;
+
+        minX = cam.x * cam.minZoom;
+        minY = cam.y * cam.minZoom;
+        minZ = cam.z * cam.minZoom;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            cam.x = cam.x * 1.1f;
-            cam.y = cam.y * 1.1f;
-            cam.z = cam.z * 1.1f;
+            cam.x = Mathf.Max(cam.x * 1.1f, maxX);
+            cam.y = Mathf.Min(cam.y * 1.1f, maxY);
+            cam.z = Mathf.Min(cam.z * 1.1f, maxZ);
         } else if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            cam.x = cam.x * .9f;
-            cam.y = cam.y * .9f;
-            cam.z = cam.z * .9f;
+            cam.x = Mathf.Min(cam.x * 0.9f, minX);
+            cam.y = Mathf.Max(cam.y * 0.9f, minY);
+            cam.z = Mathf.Max(cam.z * 0.9f, minZ);
         }
         transform.position = new Vector3(
                                         playerTransform.position.x + cam.x,
