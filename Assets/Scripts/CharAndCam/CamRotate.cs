598786974e56d6628b42c98394b85c2f213865c5
tommyvanduyne <tommyvanduyne@gmail.com>
2016-05-17 14:17:46 -0400

    edited the cam script to work again

--- Assets/Scripts/CharAndCam/CamRotate.cs
+++ Assets/Scripts/CharAndCam/CamRotate.cs
@@ -8,14 +8,13 @@ public class CamPosition
     public float z;
     public float maxZoom;
     public float minZoom;
+
 }
 
 public class CamRotate : MonoBehaviour {
     public Transform playerTransform;
     public CamPosition cam;
     private float maxX, maxY, maxZ, minX, minY, minZ;
-    Vector3 camD;
-    float distance;
 
 	// Use this for initialization
 	void Start () {
@@ -33,28 +32,18 @@ public class CamRotate : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        camD = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-        distance = Vector3.Distance(camD, playerTransform.position);
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            Debug.Log(distance);
-            if (distance < 20)
-            {
-                cam.x = Mathf.Min(cam.x * 1.1f, maxX);
-                cam.y = Mathf.Max(cam.y * 1.1f, maxY);
-                cam.z = Mathf.Max(cam.z * 1.1f, maxZ);
-            }
+            Debug.Log("asdfasdfasdf");
+            cam.x = Mathf.Max(cam.x * 1.1f, maxX);
+            cam.y = Mathf.Min(cam.y * 1.1f, maxY);
+            cam.z = Mathf.Min(cam.z * 1.1f, maxZ);  
            
         } else if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            Debug.Log(distance);
-            if (distance > 4)
-            {
-                cam.x = Mathf.Min(cam.x * 0.9f, minX);
-                cam.y = Mathf.Max(cam.y * 0.9f, minY);
-                cam.z = Mathf.Max(cam.z * 0.9f, minZ);
-            }
-           
+            cam.x = Mathf.Min(cam.x * 0.9f, minX);
+            cam.y = Mathf.Max(cam.y * 0.9f, minY);
+            cam.z = Mathf.Max(cam.z * 0.9f, minZ);
         }
         transform.position = new Vector3(
                                         playerTransform.position.x + cam.x,
