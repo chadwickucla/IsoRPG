a9fbd2c497aa5cf69787a80d4767cc3039e33bf3
tommyvanduyne <tommyvanduyne@gmail.com>
2016-05-17 17:10:24 -0400

    added a new script for the Enemy sight

---
+++ Assets/EnemySight.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour {
+    public float fieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+    private NavMeshAgent nav;
+    private GameObject player;
+    private LayerMask mask = -1;
+
+    private Vector3 previousSighting;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        personalLastSighting = new Vector3 (0,0,0);
+        
+    }
+    void Update()
+    {
+        if (playerInSight)
+        {
+            personalLastSighting = player.transform.position;
+        }
+        //if (nav.remainingDistance > .5)
+        //{
+            nav.SetDestination(personalLastSighting);
+        //}
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInSight = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInSight = false;
+        }
+    }
+}
